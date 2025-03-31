28933ffd8d73934f3ef943f21f2df967fb10734f
EthanKaull <123032101+EthanKaull@users.noreply.github.com>
2025-03-31 17:12:22 -0600

    HW11

--- HW11/js/main.js
+++ HW11/js/main.js
@@ -1,6 +1,6 @@
 const canvas = document.getElementById("myCanvas");
 const ctx = canvas.getContext("2d");
-let square1, square2;
+let mainSquare, secondarySquare;
 const startPos1 = { x: 50, y: 50 };
 const startPos2 = { x: 500, y: 500 };
 let dx = 2;
@@ -14,30 +14,29 @@ $(document).ready(() => {
     $(document).keypress(handleKeyPress);
 });
 function createSquares() {
-    square1 = new Square(startPos1.x, startPos1.y, 20, 20, "purple");
-    square2 = new Square(startPos2.x, startPos2.y, 50, 50, "yellow");
+    mainSquare = new Square(startPos1.x, startPos1.y, 20, 20, "purple");
+    secondarySquare = new Square(startPos2.x, startPos2.y, 50, 50, "yellow");
 }
 function animateGreenSquare() {
-    // Bounce logic
-    if (square2.theX + square2.theWidth >= canvas.width || square2.theX <= 0) {
+    if (secondarySquare.theX + secondarySquare.theWidth >= canvas.width || secondarySquare.theX <= 0) {
         dx = -dx;
     }
-    if (square2.theY + square2.theHeight >= canvas.height || square2.theY <= 0) {
+    if (secondarySquare.theY + secondarySquare.theHeight >= canvas.height || secondarySquare.theY <= 0) {
         dy = -dy;
     }
-    square2.setX(square2.theX + dx);
-    square2.setY(square2.theY + dy);
+    secondarySquare.setX(secondarySquare.theX + dx);
+    secondarySquare.setY(secondarySquare.theY + dy);
     drawSquares();
     requestAnimationFrame(animateGreenSquare);
 }
 function drawSquares() {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
 
-    ctx.fillStyle = square1.theColor;
-    ctx.fillRect(square1.theX, square1.theY, square1.theWidth, square1.theHeight);
+    ctx.fillStyle = mainSquare.theColor;
+    ctx.fillRect(mainSquare.theX, mainSquare.theY, mainSquare.theWidth, mainSquare.theHeight);
 
-    ctx.fillStyle = square2.theColor;
-    ctx.fillRect(square2.theX, square2.theY, square2.theWidth, square2.theHeight);
+    ctx.fillStyle = secondarySquare.theColor;
+    ctx.fillRect(secondarySquare.theX, secondarySquare.theY, secondarySquare.theWidth, secondarySquare.theHeight);
 }
 function handleKeyPress(event) {
     const key = String.fromCharCode(event.which || event.keyCode).toLowerCase();
@@ -45,23 +44,23 @@ function handleKeyPress(event) {
     else if (key === "s") moveDown();
     else if (key === "a") moveLeft();
     else if (key === "d") moveRight();
-    if (hasCollided(square1, square2)) {
+    if (hasCollided(mainSquare, secondarySquare)) {
         changeBackgroundColor();
         adjustSizes();
     }
     drawSquares();
 }
 function moveUp() {
-    square1.setY(square1.theY - 10);
+    mainSquare.setY(mainSquare.theY - 10);
 }
 function moveDown() {
-    square1.setY(square1.theY + 10);
+    mainSquare.setY(mainSquare.theY + 10);
 }
 function moveLeft() {
-    square1.setX(square1.theX - 10);
+    mainSquare.setX(mainSquare.theX - 10);
 }
 function moveRight() {
-    square1.setX(square1.theX + 10);
+    mainSquare.setX(mainSquare.theX + 10);
 }
 function changeBackgroundColor() {
     const r = Math.floor(Math.random() * 255);
@@ -70,10 +69,10 @@ function changeBackgroundColor() {
     canvas.style.backgroundColor = `rgb(${r}, ${g}, ${b})`;
 }
 function adjustSizes() {
-    square1.setWidth(square1.theWidth - 1);
-    square1.setHeight(square1.theHeight - 1);
-    square2.setWidth(square2.theWidth + 1);
-    square2.setHeight(square2.theHeight + 1);
+    mainSquare.setWidth(mainSquare.theWidth - 1);
+    mainSquare.setHeight(mainSquare.theHeight - 1);
+    secondarySquare.setWidth(secondarySquare.theWidth + 1);
+    secondarySquare.setHeight(secondarySquare.theHeight + 1);
 }
 function hasCollided(obj1, obj2) {
     return !(
